ab705ae2650dbcc34aad5855b14b524f1b2b2897
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-08-14 12:43:05 -0300

    fixes

--- src/flext_meltano/_models/core.py
+++ src/flext_meltano/_models/core.py
@@ -6,8 +6,7 @@ from typing import Annotated
 
 from flext_cli import m, u
 
-from flext_core import r
-from flext_meltano import t
+from flext_meltano import r, t
 
 
 class FlextMeltanoModelsCore:
--- src/flext_meltano/_models/payloads_data.py
+++ src/flext_meltano/_models/payloads_data.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-from collections.abc import Mapping, Sequence
+from collections.abc import Mapping
 from pathlib import Path
 from types import MappingProxyType
 from typing import Annotated
