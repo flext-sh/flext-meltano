9d3f6207b77eaa3dd7d8da730755823d36e72756
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-21 12:30:08 -0300

    chore(gen): converge lazy-init projections after identifier filter

--- examples/__init__.py
+++ examples/__init__.py
@@ -9,7 +9,8 @@ from typing import TYPE_CHECKING
 from flext_core.lazy import build_lazy_import_map, install_lazy_exports
 
 if TYPE_CHECKING:
-    from flext_meltano import FlextMeltanoConstants, d, e, h, r, s, x
+    from flext_core import d, e, h, r, x
+    from flext_meltano import FlextMeltanoConstants, s
 
     from .constants import (
         ExamplesFlextMeltanoConstants,
@@ -53,7 +54,8 @@ _LAZY_IMPORTS = MappingProxyType(
             ".protocols": ("ExamplesFlextMeltanoProtocols", "p"),
             ".typings": ("ExamplesFlextMeltanoTypes", "t"),
             ".utilities": ("ExamplesFlextMeltanoUtilities", "u"),
-            "flext_meltano": ("FlextMeltanoConstants", "d", "e", "h", "r", "s", "x"),
+            "flext_core": ("d", "e", "h", "r", "x"),
+            "flext_meltano": ("FlextMeltanoConstants", "s"),
         }),
         alias_groups=MappingProxyType({}),
         sort_keys=False,
--- src/flext_meltano/__init__.py
+++ src/flext_meltano/__init__.py
@@ -20,7 +20,7 @@ from .__version__ import (
 )
 
 if TYPE_CHECKING:
-    from flext_cli import d, e, h, r, x
+    from flext_core import d, e, h, r, x
 
     from . import services
     from .__version__ import FlextMeltanoVersion
@@ -189,7 +189,7 @@ _LAZY_IMPORTS = MappingProxyType(
             ".services.validators": ("FlextMeltanoValidators",),
             ".typings": ("FlextMeltanoTypes", "t"),
             ".utilities": ("FlextMeltanoUtilities", "u"),
-            "flext_cli": ("d", "e", "h", "r", "x"),
+            "flext_core": ("d", "e", "h", "r", "x"),
         }),
         alias_groups=MappingProxyType({}),
         sort_keys=False,
