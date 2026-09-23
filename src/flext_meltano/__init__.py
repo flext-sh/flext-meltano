cd639b3ab094536b37848abfb55bbf8e34654ad5
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-23 01:19:51 -0300

    fixes

--- src/flext_meltano/__init__.py
+++ src/flext_meltano/__init__.py
@@ -20,7 +20,9 @@ from .__version__ import (
 )
 
 if TYPE_CHECKING:
-    from flext_cli import cli, core, d, e, h, lazy_attribute, r, x
+    from flext_cli import cli
+
+    from flext_core import core, d, e, h, lazy_attribute, r, x
 
     from . import services
     from ._config import FlextMeltanoConfig, config
@@ -191,7 +193,8 @@ _LAZY_IMPORTS = MappingProxyType(
             ".services.validators": ("FlextMeltanoValidators",),
             ".typings": ("FlextMeltanoTypes", "t"),
             ".utilities": ("FlextMeltanoUtilities", "u"),
-            "flext_cli": ("cli", "core", "d", "e", "h", "lazy_attribute", "r", "x"),
+            "flext_cli": ("cli",),
+            "flext_core": ("core", "d", "e", "h", "lazy_attribute", "r", "x"),
         }),
         alias_groups=MappingProxyType({}),
         sort_keys=False,
